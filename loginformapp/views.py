6c7864c6e0e65a2ff96fd4218a88d9b94cde7b9e
smarak1996 <smarakprd5@gmail.com>
2022-01-22 15:52:06 +0530

    commiting here

--- loginformapp/views.py
+++ loginformapp/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from .models import LoginData
+from django.contrib import messages 
 
 
 def login_view(request):
     if request.method == 'POST':
-        LoginData(
+        
         email = request.POST['email'],
         password = request.POST['password']
-        ).save()
 
-        return render(request, 'login.html')
+        if LoginData.objects.filter(email = email).exists():
+            messages.warning(request, 'Email already exists!!!')
+            return render(request, 'login.html')
+
+        else:
+
+            LoginData(
+                email = email,
+                password = password
+            ).save()
+            messages.success(request, 'Login Successfull!!!')
+            return render(request, 'login.html')
+
     
     else:
 
